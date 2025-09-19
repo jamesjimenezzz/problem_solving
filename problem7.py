5dd03b15ce4ba2b31063f4665b1dff6b889ecc0e
jamesjimenezzz <asunahaley@gmail.com>
2025-09-20 00:39:40 +0800

    added another problem

---
+++ problem7.py
@@ -0,0 +1,10 @@
+#SOLVED
+#MULTIPLICATION TABLE
+
+
+number = int(input("Enter a number to create multiplication table: "))
+
+
+for i in range(0, 10 + 1):
+    print(f"{number} * {i} = {number * i}")
+
